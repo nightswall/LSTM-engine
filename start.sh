717a87f9ab8f83dd31c5bcd556e0f110f97dd2e7
mohamed <e2044725@ceng.metu.edu.tr>
2023-05-08 15:23:19 +0300

    tested

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ rm myapp/lstm_model_temperature_9.pt
 rm tempNewData.csv
 rm h_tensor.pt
 rm temperature_data.npz
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000 --noreload
